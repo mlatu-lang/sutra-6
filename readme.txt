4babf67c2189690c9084e7e022570e7289144bed
Garklein <garklein97@gmail.com>
2025-05-08 19:55:36 -0400

    fix segfault

--- readme.txt
+++ readme.txt
@@ -9,3 +9,5 @@ find: brute-force the shortest combinator with a certain effect
 bb: brute-force busy beaver programs
 
 the macro LIM in sutra-6.c is very important. it controls how long programs run before sutra-6 kills them, and the amount of stack space they get. increase this if you're brute-forcing busy beavers.
+
+testing: make bible && ./bible 5 > b5 && diff b5 test/b5
--- sutra-6.c
+++ sutra-6.c
@@ -11,13 +11,13 @@ typedef struct a { I u; struct a *n, *a; } *A; I SZA=sizeof(struct a); // arena 
 #define LIM 1000 // max # of iterations before giving up, also max stack size before giving up
 #define AS 2048 // arena size
 A nA() { A a=malloc(AS); a->a=a; a->u=0; a->n=0; R a; }
-V*ma(I l,A m) { A a=m->a; if (l>AS-a->u-SZA) { if (!a->n) a->n=nA(); a=m->a=a->n; a->u=0; } V*p=(char*)a+a->u+SZA; a->u+=l; R p; }
+V*ma(I l,A r) { A a=r->a; if (l>AS-a->u-SZA) { if (!a->n) a->n=nA(); a=r->a=a->n; a->u=0; } V*p=(char*)a+a->u+SZA; a->u+=l; R p; }
 V rc(A a) { a->u=0; a->a=a; } // free memory to allocator (reclaim)
 V fr(A a) { while (a) { A n=a->n; free(a); a=n; } } // free memory to os
 
 // mlatu term - type, length. if CHR, c is the character. if ARR, a is the array. if CAT, x/y are the halves.
 enum T { CHR,ARR,CAT }; typedef struct m { enum T t; I l; char c; struct m **a, *x, *y; } *M;
-M nM(enum T t,A a) { M m=ma(sizeof(struct m),a); m->t=t; R m; } M nMC(char c,A a) { M m=nM(CHR,a); m->c=c; R m; }
+M nM(enum T t,A a) { M m=ma(sizeof(struct m),a); m->t=t; m->l=0; R m; } M nMC(char c,A a) { M m=nM(CHR,a); m->c=c; R m; }
 I pr(M m,I p) { P(!m, 0); if (m->t==CHR) PF("%c",m->c); // print
 	else { p&&PF("("); if (m->t==ARR) DO(m->l, pr(m->a[i],1)); else pr(m->x,0), pr(m->y,0); p&&PF(")"); }; }
 
@@ -25,9 +25,9 @@ S sp(S s) { I p=0; for (;*s;s++) { p+=(*s=='(')-(*s==')'); P(!p, s); } R s; } //
 I len(S s) { I l=0; for (;*s;s++) { P(*s==')', l); l++; P(!*(s=sp(s)), l); } R l; }
 M ps(S s,I p,A a); M pq(S s,I p,A a) { M z=ps(s+1,p+1,a); P(!z||!*sp(s), 0); R z; } // parse, parse quote
 M ps(S s,I p,A a) { M m=nM(ARR,a); m->l=len(s); m->a=ma(m->l*sizeof(M),a); // returns null on unmatched parens
-	DO(m->l, P(*s==')', !p?0:m); P(!(m->a[i]=*s=='('?pq(s,p,a):nMC(*s,a)), 0); P(!*(s=sp(s)), 0); s++); P(!p&&*s, 0); R m; }
+	DO(m->l, P(*s==')', !p?0:m); P(!(m->a[i]=*s=='('?pq(s,p,a):nMC(*s,a)), 0); P(!*(s=sp(s)), 0); s++); R m; }
 
-#define FIX(ip)   ((ip)+LIM)%LIM
+#define FIX(ip)   ((ip)+LIM)%LIM // wraparound
 #define TOS       s->a[s->l-1]
 #define PUSH(elt) s->a[s->l++]=elt
 #define POP       s->a[--s->l]
@@ -36,17 +36,16 @@ M ps(S s,I p,A a) { M m=nM(ARR,a); m->l=len(s); m->a=ma(m->l*sizeof(M),a); // re
 #define Q2        s->l>1&&Q(TOS)&&Q(s->a[s->l-2])
 #define PRM(chr,cnd,act) case chr: if (cnd) { (*l)++; act; R 1; } R 0;
 I uw(M m,M p,I ip) { P(m->t==CAT, ip=uw(m->x,p,ip), uw(m->y,p,ip)); DO(m->l, p->a[ip]=m->a[i]; ip=FIX(ip+1)); R ip; }
-I chr(char c,M s,M p,I*ip,I*l,A a) { switch (c) {
+I chr(char c,M s,M p,I*ip,I*l,A a) { switch (c) { // 1 = rewrite, 0 = no rewrite, -1 = exceeds limit
 	PRM('+', Q1, PUSH(TOS));
 	PRM('-', Q1, POP);
 	PRM('>', Q1, M m=nM(ARR,a); m->l=1; m->a=ma(sizeof(M),a); *m->a=TOS; TOS=m);
 	PRM('~', Q2, M y=POP; M x=POP; PUSH(y); PUSH(x));
-	PRM(',', Q2, M y=POP; M x=TOS; P(!y->l, 1); P(!x->l, TOS=y, 1); M m=nM(CAT,a); m->x=x; m->y=y; m->l=x->l+y->l; TOS=m);
-	// catting to an empty quote is the other quote. thus, if a cat has length 0 (and ofc if <0), the length has overflowed
-	PRM('<', Q1, M m=POP; P(m->t==CAT&&m->l<=0, p->l+=1+LIM, 1); P(m->l>LIM, 1); *ip=FIX(*ip-m->l); uw(m,p,FIX(*ip+1)); p->l+=m->l);
-	default: R 0; } }
-I ex1(M s,M p,I*ip,I*l,A a) { P(!p->l,1); M m=p->a[*ip]; if (m->t!=CHR||!chr(m->c,s,p,ip,l,a)) PUSH(m);
-	P(p->l+s->l>LIM,*l=-1); p->l--; *ip=FIX(*ip+1); R !p->l; }
+	PRM(',', Q2, M y=POP; M x=TOS; P(!y->l, 1); P(!x->l, TOS=y, 1); M m=TOS=nM(CAT,a); m->x=x; m->y=y; m->l=x->l+y->l; P(m->l<=0, -1));
+	PRM('<', Q1, M m=POP; P(m->l>LIM, -1); *ip=FIX(*ip-m->l); uw(m,p,FIX(*ip+1)); p->l+=m->l); }
+	R 0; }
+I ex1(M s,M p,I*ip,I*l,A a) { P(!p->l,1); M m=p->a[*ip]; I r=0; if (m->t!=CHR||!(r=chr(m->c,s,p,ip,l,a))) PUSH(m);
+	P(r<0||p->l+s->l>LIM,*l=-1); *ip=FIX(*ip+1); R !(--p->l); }
 M pa[LIM], sa[LIM];
-M ex(M p,I*l,A a) { M*oa=p->a; p->a=pa; DO(p->l, p->a[i]=oa[i]); M s=nM(ARR,a); s->l=0; s->a=sa; // l: # of rewrites / -1 if gave up
+M ex(M p,I*l,A a) { M*oa=p->a; p->a=pa; DO(p->l, p->a[i]=oa[i]); M s=nM(ARR,a); s->a=sa; // l: # of rewrites / -1 if gave up
 	I ip=*l=0; while (*l<LIM) P(ex1(s,p,&ip,l,a), s); *l=-1; R s; }
